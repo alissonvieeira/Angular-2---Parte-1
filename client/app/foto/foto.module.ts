ed234c705b2e228b1f4276e7c2dcf712446ab96e
Alisson Vieira <alisson.echo@gmail.com>
2018-05-29 23:44:26 -0300

    Consumindo uma API

---
+++ client/app/foto/foto.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { FotoComponet } from './foto.component';
+
+@NgModule({
+    declarations: [ FotoComponet ],
+    exports: [ FotoComponet ]
+})
+
+export class FotoModule {}
